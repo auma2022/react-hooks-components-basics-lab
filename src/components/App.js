94b45396bf303da2467751ed9fdb1aa9879fd9c9
auma2022 <auma.amwayi@student.moringaschool.com>
2022-07-20 11:27:58 +0300

    add comments

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,75 @@
 import React from "react";
+/*
+
+
+    import React from 'react';
+    import { NavLink as Link } from 'react-router-dom';
+    
+
+2.
+
+    <nav>
+      <Link to="/">I'm a link!</Link>
+    </nav>
+    )
+    
+
+3.
+
+    // ...
+    <Link to="/">I'm a link!</Link>
+    
+4.
+
+    // ...
+    <li>
+      <Link to="/">
+        Home
+      </Link>
+    </li>
+    
+5.
+
+    // ...
+    <li>
+      <Link to="/about">
+        About
+      </Link>
+    </li>
+    
+6.
+
+    // ...
+    <li>
+      <Link to="/contact">
+        Contact
+      </Link>
+    </li>
+    
+7.
+
+    // ...
+    import { useSelector, useDispatch } from 'react-redux'
+    import { RootState } from '../store'
+    import { selectPage } from '../store/navbar/actions'
+    
+8.
+
+    // ...
+
+    const dispatch = useDispatch()
+
+    const nav = useSelector(({ navbar }: RootState) => {
+      const { page } = navbar;
+      return { page };
+    })
+
+    const handleNavClick = (page: string) => {
+      dispatch(selectPage(page))
+    }
+
+  
+ */
 
 function NavBar() {
   return (
